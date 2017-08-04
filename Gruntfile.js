ab09c031e8dcba3a3c40c29e6ec4dab8e07439d6
BartPu <bpuchalski@tlen.pl>
2017-08-04 14:41:28 +0200

    third commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,31 +2,14 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        sass: {
-            options: {
-                sourceMap: true
-            },
-            dist: {
-                files: {
-                    'css/main.css': 'sass/main.sass'
-                }
-            }
-        },
-        watch: {
-            scripts: {
-                files: ['sass/*.sass'],
-                tasks: ['sass'],
-                options: {
-                    spawn: false,
-                }
-            }
-        },
+
         browserSync: {
             dev: {
                 bsFiles: {
                     src: [
                         'css/*.css',
-                        '*.html'
+                        '*.html',
+                        'js/*.js'
                     ]
                 },
                 options: {
@@ -37,10 +20,8 @@ module.exports = function(grunt) {
         },
     });
     // Load the plugins tasks
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browser-sync')
     // Default task(s).
 
-    grunt.registerTask('default', ['browserSync', 'sass', 'watch']);
+    grunt.registerTask('default', ['browserSync']);
 };
